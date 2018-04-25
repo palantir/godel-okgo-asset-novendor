3d28f7e37d15923318d3f7c27eaae78e4c8f9f90
Nick Miyake <nmiyake@palantir.com>
2018-04-25 16:33:52 -0700

    Update cobra CLI version

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func main() {
 
 func checkMain(osArgs []string) int {
 	os.Args = osArgs
-	var debugFlagVal bool
 	rootCmd := checker.AssetRootCmd(creator.Novendor(), config.UpgradeConfig, "run novendor check")
-	return cobracli.ExecuteWithDefaultParamsWithVersion(rootCmd, &debugFlagVal, "")
+	return cobracli.ExecuteWithDefaultParams(rootCmd)
 }
